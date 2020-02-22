1f402245b9c4b8c45eb5ff5579ea5b81db21a869
jwoodii <35505841+jwoodii@users.noreply.github.com>
2020-02-21 14:06:35 -1000

    Moved functions to cxx files
    Moved linked list and node function to seperate .cxx files

---
+++ MSVC/build/LinkedList.cxx
@@ -0,0 +1,46 @@
+#include "LinkedList.h"
+#include "Node.h"
+LinkedList::LinkedList(Node start) {
+    this->Head = &start;
+    this->Tail = &start;
+}
+Node LinkedList::lowestSearch() {
+    Node current = *Head;
+    Node lowest = *Head;
+    while (current.hasChild()) {
+        if (lowest.getWeight() > current.getWeight()) {
+            lowest = current;
+        }
+        current = *current.getChild();
+    }
+    return lowest;
+}
+void LinkedList::addNode(Node add) {
+    Tail->setChild(add);
+    Tail = &add;
+}
+Node LinkedList::remNode(Node rem) {
+    Node current = *Head;
+    Node prev;
+    while (current != rem) {
+        prev = current;
+        current = *current.getChild();
+    }
+    prev.setChild(*current.getChild());
+    return current;
+}
+
+bool LinkedList::isEmpty() {
+    if (Head != NULL) {
+        return false;
+    }
+    return true;
+}
+std::string LinkedList::printList() {
+    std::string results = "";
+    Node current = *Head;
+    while (current.hasChild()) {
+        results += "(" + current.getX + "," + current.getY() + "," + current.getDistanceToGoal() + "," + current.getWeight() + ")";
+    }
+}
+;

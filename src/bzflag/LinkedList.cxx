3f21c56c62b642f820cdc390c931f0d38f409d27
jwoodii <35505841+jwoodii@users.noreply.github.com>
2020-02-21 15:20:48 -1000

    ummm

--- src/bzflag/LinkedList.cxx
+++ src/bzflag/LinkedList.cxx
@@ -1,100 +1,7 @@
-/************************************************
-*Node functions from Node.h
-* includes constructors, getter/setters, and hasChild
-*
-*************************************************/
-
-
-#include "Node.h"
-Node::Node() {
-    this->x = 0;
-    this->y = 0;
-    this->child = nullptr;
-    this->childExists = false;
-    this->distanceTraveled = 0;
-    this->weight = 0;
-}
-Node::Node(int x, int y, int distanceTraveled, int distToGoal) {
-    this->x = x;
-    this->y = y;
-    this->distanceTraveled = distanceTraveled;
-    this->weight = distanceTraveled + distToGoal;
-    this->childExists = false;
-    this->child = nullptr;
-}
-Node::Node(int x, int y, int distance, int toGoal, Node child)
-{
-    this->x = x;
-    this->y = y;
-    this->distanceTraveled = distance;
-    this->weight = distance + toGoal;
-    this->child = &child;
-    this->childExists = true;
-}
-bool Node::operator==(Node check) {
-    if (x = check.getX()) {
-        if (y = check.getX()) {
-            if (child = check.getChild()) {
-                if (distanceTraveled = check.getDistanceTraveled()) {
-                    if (weight = check.getWeight()) {
-                        return true;
-                    }
-                }
-            }
-        }
-    }
-    return false;
-}
-bool Node::operator!=(Node check) {
-    if (x = check.getX()) {
-        if (y = check.getX()) {
-            if (child = check.getChild()) {
-                if (distanceTraveled = check.getDistanceTraveled()) {
-                    if (weight = check.getWeight()) {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-    return true;
-}
-bool Node::hasChild() {
-    return childExists;
-}
-int Node::getX()
-{
-    return x;
-}
-
-int Node::getY() {
-    return y;
-}
-void Node::setChild(Node child) {
-    if (&child == nullptr) {
-        childExists = false;
-        this->child = false;
-    }
-    else {
-        this->child = &child;
-        this->childExists = true;
-    }
-}
-Node* Node::getChild() {
-    return child;
-}
-int Node::getDistanceTraveled() {
-    return distanceTraveled;
-}
-int Node::getDistanceToGoal() {
-    return weight - distanceTraveled;
-}
-int Node::getWeight() {
-    return weight;
-};
 #pragma once
 #include "Node.h"
 #include <string>
+#include "LinkedList.h"
 class LinkedList {
 public:
     Node lowestSearch();
@@ -152,7 +59,12 @@ std::string LinkedList::printList() {
     std::string results = "";
     Node current = *Head;
     while (current.hasChild()) {
-        results += "(" + current.getX + "," + current.getY() + "," + current.getDistanceToGoal() + "," + current.getWeight() + "," ")";
     }
+    return results;
 }
+
+void LinkedList::addOrdered(Node add)
+{
+}
+
 ;
--- src/bzflag/LinkedList.h
+++ src/bzflag/LinkedList.h
@@ -9,7 +9,7 @@ public:
     void addNode(Node add);
     Node remNode(Node rem);
     std::string printList();
-    void addInOrder(Node add);
+    void addOrdered(Node add);
 private:
     Node* Head;
     Node* Tail;

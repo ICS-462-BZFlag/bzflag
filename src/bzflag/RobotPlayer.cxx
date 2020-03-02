8cb3f1801f9c22169d2db8975b7255f976be68ba
Jeffery Wood <35505841+jwoodii@users.noreply.github.com>
2020-03-01 19:50:52 -1000

    Clean Up

--- src/bzflag/RobotPlayer.cxx
+++ src/bzflag/RobotPlayer.cxx
@@ -381,9 +381,6 @@ void            RobotPlayer::doUpdateMotion(float dt)
                 aStar(position, path, goalPath);
                 flip = true;
             }
-            //aSearch(intPosition, intPath, goalPath);
-            //goalPath.insertInOrder(3, 3, 3, 3);
-            //goalPath.printList();
             //Node next = *goalPath.pop();
             //goalPath.print(&next);
             //intPath[0] = next.x;
@@ -951,23 +948,6 @@ Player*		RobotPlayer::lookupLocalPlayer(PlayerId id)
 /* end of lines added by David Chin */
 //Start of lines added by Group
 
-/*
-*generateDescendant
-*given a parent node, the directions of movement in x and y
-*and the goal, it will create a new node based on the movement from parent Node
-*/
-Node RobotPlayer::generateDescendant(Node parent, int addX, int addY, int goal[2])
-{
-    Node* new_node = new Node();
-    new_node->x = parent.x + addX;
-    new_node->y = parent.y + addY;
-    new_node->distanceTraveled = parent.distanceTraveled + (int)hypotf(parent.x + addX,parent.y + addY);
-    new_node->distanceToGoal = hypotf(goal[0] - new_node->x, goal[1] - new_node->y);
-    new_node->weight = new_node->distanceTraveled + new_node->distanceToGoal;
-    new_node->parent = &parent;
-    return *new_node;
-
-}
 bool RobotPlayer::isLegal(int x, int y) {
     float f[2];
     f[0] = (float)x;
@@ -1102,87 +1082,6 @@ void RobotPlayer::aStar(float start[2], float goal[2], std::vector<Node*> path) 
     }
    // RobotPlayer::printQueue(open);
 }
-/*
-aSearch takes a start position, goal position and a LinkedList path and sets path to the shortest path found.
-*/
-//void RobotPlayer::aSearch(int start[2], int goal[2], LinkedList path)
-//{
-//    bool finished = false;
-//    LinkedList open = LinkedList();
-//    LinkedList closed = LinkedList();
-//    Node* current = nullptr;
-//   // open.add(start[0], start[1], 0, hypotf((goal[0] - start[0]), (goal[1] - start[1])));
-//    char buffer[128];
-//    sprintf(buffer, "called 1");
-//    controlPanel->addMessage(buffer);
-//    memset(buffer, 0, sizeof(buffer));
-//    while (!open.isEmpty() && !finished) { //  while (open.isEmpty() == false && finished == false) {
-//        current = open.pop();
-//        sprintf(buffer, "called 2");
-//        controlPanel->addMessage(buffer);
-//        memset(buffer, 0, sizeof(buffer));
-//        if (current->x == goal[0] && current->y == goal[1]) {
-//            finished = true;
-//            sprintf(buffer, "Called 3");
-//            controlPanel->addMessage(buffer);
-//            memset(buffer, 0, sizeof(buffer));
-//        }
-//        else {
-//            sprintf(buffer, "called 4");
-//            controlPanel->addMessage(buffer);
-//            memset(buffer, 0, sizeof(buffer));
-//            for (int i = -1; i <= 1; i++) {
-//                for (int j = -1; i <= 1; i++) {
-//                    //for each node_successor of node_current
-//                    Node node_successor = generateDescendant(*current, i, j, goal);
-//                    node_successor.parent = current;
-//                    //if node_successor is in the OPEN list {
-//                    if (open.contains(node_successor)) {
-//                        //if g(node_successor) < successor_current_cost continue
-//                        if (node_successor.distanceTraveled <= current->distanceTraveled) {
-//                            goto line20;
-//                        }
-//                    }
-//                    // else if node_successor is in the CLOSED list{
-//                    else if (closed.contains(node_successor)) {
-//                        //if g(node_successor) <= successor_current_cost
-//                        if (node_successor.distanceTraveled <= current->distanceTraveled) {
-//                            goto line20;
-//                        }
-//                        //Move node_successor from the CLOSED list to the OPEN list
-//                        closed.remove(node_successor.x,node_successor.y);
-//                        open.insertInOrder(node_successor.x, node_successor.y, node_successor.distanceTraveled, node_successor.distanceToGoal);
-//                    }
-//                    else {
-//                        //Add node_successor to the OPEN list
-//                        open.insertInOrder(node_successor.x, node_successor.y, node_successor.distanceTraveled, node_successor.distanceToGoal);
-//                        //Set h(node_successor) to be the heuristic distance to node_goal
-//                        node_successor.weight = node_successor.distanceTraveled + (int)hypotf(goal[0] - node_successor.x, goal[1] - node_successor.y);
-//
-//                    }
-//                    //Set g(node_successor) = successor_current_cost
-//                    node_successor.weight = current->weight;
-//                    //Set the parent of node_successor to node_current
-//                    node_successor.parent = current;
-//                    current->child = &node_successor;
-//
-//                    //parent?
-//                }
-//            line20:
-//                //Add node_current to the CLOSED list
-//                closed.insertInOrder(current->x, current->y, current->distanceTraveled, current->distanceToGoal);
-//
-//            }
-//        }
-//    }
-//    ////if (node_current != node_goal) exit with error(the OPEN list is empty)
-//    while (current->parent != nullptr) {
-//        path.add(current->x,current->y,current->distanceTraveled,current->distanceToGoal);
-//        current = current->parent;
-//    }
-//    //return *new LinkedList;
-//
-//}
 
 // Local Variables: ***
 // mode: C++ ***

cf15f4e718d92da35872704217355c460bab01a4
kainyogi <kainyogi@hawaii.edu>
2020-03-04 15:15:35 -1000

    File push

--- src/bzflag/RobotPlayer.h
+++ src/bzflag/RobotPlayer.h
@@ -10,18 +10,16 @@
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 
- /*
-  *
-  */
+/*
+ *
+ */
 
 #ifndef BZF_ROBOT_PLAYER_H
 #define BZF_ROBOT_PLAYER_H
 
 #include "common.h"
-#include "LinkedList.h"
-#include "Node.h"
 
-  /* system interface headers */
+/* system interface headers */
 #include <vector>
 
 /* interface header */
@@ -32,67 +30,75 @@
 #include "RegionPriorityQueue.h"
 #include "ServerLink.h"
 
-#include<queue>
-
+/* lines added by David Chin */
+#include "AStarNode.h" // needed for A* search
+/* end of lines added by David Chin */
 
 class RobotPlayer : public LocalPlayer
 {
 public:
     RobotPlayer(const PlayerId&,
-        const char* name, ServerLink*,
-        const char* _motto);
+                const char* name, ServerLink*,
+                const char* _motto);
 
     float       getTargetPriority(const Player*) const;
-    const Player* getTarget() const;
+    const Player*   getTarget() const;
     void        setTarget(const Player*);
     static void     setObstacleList(std::vector<BzfRegion*>*);
 
     void        restart(const float* pos, float azimuth);
     void        explodeTank();
-	bool isLegal(int x, int y);
-    void scaleUp(float pos[2], float newPos[2]);
-	void scaleDown(float pos[2], float newPos[2]);
-	Node GenerateNode(Node* parent, int x, int y, float distanceTrav, float distanceGoal);
-    void PopACertainNode(Node node, std::priority_queue<Node> open);
-    bool IsInQueue(Node node, std::priority_queue<Node> open, Node* returnMe);
-    void printQueue(std::priority_queue<Node*> open);
-    void aStar(float start[2], float goal[2], std::vector<Node> path);
+/* code added by David Chin */
+	bool		RobotPlayer::amAlive(float dt);
+	bool		returnTrue(float dt);
+	bool		returnFalse(float dt);
+	void		RobotPlayer::a1(float dt);
+	void		RobotPlayer::a2(float dt);
+	void		RobotPlayer::a3(float dt);
+	void		RobotPlayer::a4(float dt);
+	void		RobotPlayer::a5(float dt);
+/* end of code added by David Chin */
 
 private:
     void        doUpdate(float dt);
     void        doUpdateMotion(float dt);
-    BzfRegion* findRegion(const float p[2], float nearest[2]) const;
+    BzfRegion*      findRegion(const float p[2], float nearest[2]) const;
     float       getRegionExitPoint(
         const float p1[2], const float p2[2],
         const float a[2], const float targetPoint[2],
         float mid[2], float& priority);
     void       findPath(RegionPriorityQueue& queue,
-        BzfRegion* region, BzfRegion* targetRegion,
-        const float targetPoint[2], int mailbox);
+                        BzfRegion* region, BzfRegion* targetRegion,
+                        const float targetPoint[2], int mailbox);
 
-    void       projectPosition(const Player* targ, const float t, float& x, float& y, float& z) const;
-    void       getProjectedPosition(const Player* targ, float* projpos) const;
-    /* lines added by David Chin */
-    void		findHomeBase(TeamColor teamColor, float location[3]);
-    bool		myTeamHoldingOpponentFlag(void);
-    void		findOpponentFlag(float location[3]);
-    int		computeCenterOfMass(float neighborhoodSize, float cmOut[3]);
-    int		computeRepulsion(float neighborhoodSize, float repulseOut[3]);
-    int		computeAlign(float neighborhoodSize, float avVOut[3], float* avAzimuthOut);
-    Player* lookupLocalPlayer(PlayerId id);
-    static const float		CohesionW;
-    static const float		SeparationW;
-    static const float		AlignW;
-    static const float		PathW;
-    /* end of lines added by David Chin */
+    void       projectPosition(const Player *targ,const float t,float &x,float &y,float &z) const;
+    void       getProjectedPosition(const Player *targ, float *projpos) const;
+/* lines added by David Chin */
+     void		findHomeBase(TeamColor teamColor, float location[3]);
+     bool		myTeamHoldingOpponentFlag(void);
+     void		findOpponentFlag(float location[3]);
+     int		computeCenterOfMass(float neighborhoodSize, float cmOut[3]);
+     int		computeRepulsion(float neighborhoodSize, float repulseOut[3]);
+     int		computeAlign(float neighborhoodSize, float avVOut[3], float* avAzimuthOut);
+     Player* 		lookupLocalPlayer(PlayerId id);
+
+     static const float		CohesionW;
+     static const float		SeparationW;
+     static const float		AlignW;
+     static const float		PathW;
+/* end of lines added by David Chin */
 
 private:
-    const Player* target;
+    const Player*   target;
     std::vector<RegionPoint>    path;
     int         pathIndex;
     float       timerForShot;
     bool        drivingForward;
     static std::vector<BzfRegion*>* obstacleList;
+/* lines added by David Chin */
+    std::vector< AStarNode > AstarPath; // planner result path
+    AStarNode pathGoalNode;	// goal position for current planner result
+/* end of lines added by David Chin */
 };
 
 #endif // BZF_ROBOT_PLAYER_H
